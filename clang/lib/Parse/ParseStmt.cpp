a1fc37435a55b779bf27c0af816c57145d808123
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2020-04-22 17:46:31 -0700

    Run clang-format on most recent changes

--- clang/lib/Parse/ParseStmt.cpp
+++ clang/lib/Parse/ParseStmt.cpp
@@ -736,14 +736,13 @@ StmtResult Parser::ParsePatternStatement(InspectStmt *Inspect,
   return StmtError();
 }
 
-/// ParsePatternGuard - a 'if' followed by a condition, no body. 
+/// ParsePatternGuard - a 'if' followed by a condition, no body.
 ///
 ///       pattern-guard:
 ///         'if' (expression)
 ///
 bool Parser::ParsePatternGuard(Sema::ConditionResult &Cond,
-                               SourceLocation &IfLoc,
-                               bool IsConstexprIf) {
+                               SourceLocation &IfLoc, bool IsConstexprIf) {
   assert(Tok.is(tok::kw_if) && "expected 'if'");
 
   IfLoc = ConsumeToken(); // eat the 'if'.
@@ -757,7 +756,7 @@ bool Parser::ParsePatternGuard(Sema::ConditionResult &Cond,
   StmtResult InitStmt; // FIXME: not allowed in pattern matching
   if (ParseParenExprOrCondition(&InitStmt, Cond, IfLoc,
                                 IsConstexprIf ? Sema::ConditionKind::ConstexprIf
-                                            : Sema::ConditionKind::Boolean))
+                                              : Sema::ConditionKind::Boolean))
     return false;
 
   return true;
@@ -785,7 +784,7 @@ StmtResult Parser::ParseWildcardPattern(ParsedStmtContext StmtCtx) {
 
   // FIXME: retrieve constexpr information from InspectStmt
   if (Tok.is(tok::kw_if))
-    if (!ParsePatternGuard(Cond, IfLoc, false/*IsConstexprIf*/))
+    if (!ParsePatternGuard(Cond, IfLoc, false /*IsConstexprIf*/))
       return StmtError();
 
   if (!Tok.is(tok::colon)) {
@@ -838,7 +837,7 @@ StmtResult Parser::ParseIdentifierPattern(ParsedStmtContext StmtCtx) {
 
   // FIXME: retrieve constexpr information from InspectStmt
   if (Tok.is(tok::kw_if))
-    if (!ParsePatternGuard(Cond, IfLoc, false/*IsConstexprIf*/))
+    if (!ParsePatternGuard(Cond, IfLoc, false /*IsConstexprIf*/))
       return StmtError();
 
   if (!Tok.is(tok::colon)) {
@@ -860,9 +859,8 @@ StmtResult Parser::ParseIdentifierPattern(ParsedStmtContext StmtCtx) {
   if (SubStmt.isInvalid())
     SubStmt = Actions.ActOnNullStmt(ColonLoc);
 
-  return Actions.ActOnIdentifierPattern(IdentifierLoc, ColonLoc,
-                                        II, SubStmt.get(),
-                                        Cond.get().second);
+  return Actions.ActOnIdentifierPattern(IdentifierLoc, ColonLoc, II,
+                                        SubStmt.get(), Cond.get().second);
 }
 
 StmtResult Parser::ParseExpressionPattern(InspectStmt *Inspect,
